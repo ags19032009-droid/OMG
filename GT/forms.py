e620b9226181d6a46e44b8937b467bc472a82f4b
ags19032009 <ags19032009@gmail.com>
2026-06-16 16:59:46 -0300

    Atividade

--- GT/forms.py
+++ GT/forms.py
@@ -1,8 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from GT.models import Usuario
 
-class Form(FlaskForm):
-    pass
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[ DataRequired(), Email()])
+    senha = PasswordField("Senha",validators=[ DataRequired(), Length(min=6, max=20)] )
+    botao_confirmacao = SubmitField("Entrar")
+
 
 class FormCriarConta(FlaskForm):
-    pass
+    username = StringField("Nome", validators=[ DataRequired(), Length(min=2, max=50) ] )
+    email = StringField("E-mail", validators=[ DataRequired(), Email() ])
+    senha = PasswordField("Senha", validators=[ DataRequired(),Length(min=6, max=20)] )
+    cargo = SelectField("Cargo",choices=[("gerente", "Gerente"), ("funcionario", "Funcionário")],validators=[DataRequired()])
+
+    confirmacao_senha = PasswordField("Confirmar Senha",validators=[ DataRequired(), EqualTo("senha") ])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+
+        if usuario:
+            raise ValidationError( "Este e-mail já está cadastrado. Use outro e-mail.")
+
+
--- GT/routes.py
+++ GT/routes.py
@@ -1,15 +1,44 @@
-from GT import app
-from flask import render_template, url_for
-from flask_login import login_required
+from GT import app, bcrypt, database
+from flask import render_template, url_for, redirect
+from flask_login import login_required, login_user, logout_user
+from GT.forms import FormLogin, FormCriarConta
+from GT.models import Usuario
 
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    return render_template('homepage.html')
+    formlogin = FormLogin()
+    return render_template('homepage.html', form=formlogin)
+
+
+@app.route('/criarconta', methods=['GET', 'POST'])
+def criarconta():
+    formcriarconta = FormCriarConta()
+
+    if formcriarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(formcriarconta.senha.data)
+        usuario = Usuario(
+            nome=formcriarconta.username.data,
+            email=formcriarconta.email.data,
+            senha=senha,
+            cargo=formcriarconta.cargo.data
+        )
+
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for('perfil', usuario=usuario.nome))
+
+
+    return render_template(
+        'criarconta.html',form=formcriarconta)
 
 
 @app.route('/perfil/<usuario>')
 @login_required
 def perfil(usuario):
     return render_template(
-        'perfil.html',usuario=usuario)
+        'perfil.html',
+        usuario=usuario
+    )
