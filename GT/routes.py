e9fd0b35c9a00873e7800c9657a0c54bd5b6c043
ags19032009 <ags19032009@gmail.com>
2026-06-19 16:21:23 -0300

    atualização OMG

--- GT/routes.py
+++ GT/routes.py
@@ -1,6 +1,6 @@
 from GT import app, bcrypt, database
 from flask import render_template, url_for, redirect
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, current_user, logout_user
 from GT.forms import FormLogin, FormCriarConta
 from GT.models import Usuario
 
@@ -9,7 +9,18 @@ from GT.models import Usuario
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     formlogin = FormLogin()
+    if formlogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
+            login_user(usuario)
+            return redirect(url_for('perfil', id_usuario=usuario.id))
     return render_template('homepage.html', form=formlogin)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
 
 
 @app.route('/criarconta', methods=['GET', 'POST'])
@@ -24,21 +35,20 @@ def criarconta():
             senha=senha,
             cargo=formcriarconta.cargo.data
         )
-
         database.session.add(usuario)
         database.session.commit()
         login_user(usuario, remember=True)
-        return redirect(url_for('perfil', usuario=usuario.nome))
+        return redirect(url_for('perfil', id_usuario=usuario.id))
 
 
     return render_template(
         'criarconta.html',form=formcriarconta)
 
-
-@app.route('/perfil/<usuario>')
+@app.route('/perfil/<id_usuario>')
 @login_required
-def perfil(usuario):
-    return render_template(
-        'perfil.html',
-        usuario=usuario
-    )
+def perfil(id_usuario):
+    if int(id_usuario) == (current_user.id):
+        return render_template('perfil.html', usuario=current_user)
+    else:
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template('perfil.html', usuario=usuario)
